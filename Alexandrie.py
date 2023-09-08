de653b47ab43c5c2b0a5fb835d3c038376d87a0f
Sweetkoffi-PC01 <martinkoffi@proton.me>
2023-09-08 15:48:15 -0400

    First release of Hello World!

---
+++ Alexandrie.py
@@ -0,0 +1,113 @@
+#########################################################
+#  Projet : Alexandrie                                  #
+#  last update : 2023-08-09                             #
+#  Author : SweetKoffi                                  #
+#                                                       #
+#########################################################
+import requests
+import json
+import configparser
+from rich.console import Console
+from rich.table import Table
+from datetime import datetime , timedelta
+#from google_scholar_py import CustomGoogleScholarProfiles ---> Option 1
+from serpapi import GoogleSearch 
+yesterday = datetime.now() - timedelta(days = 1)
+######################################################### Config file 
+# objet de config
+config = configparser.ConfigParser()
+# Lire fichier de config
+config.read('config.txt')
+# Accéder aux API KEYS
+news_api_key = config['NEWSAPI']['api_news_key']
+serapi_api_key = config['SERPAPI']['api_serpAi_key']
+######################################################### LOGO
+logo = '''                                          
+    ___    __                          __     _  
+   /   |  / /__  _  ______ _____  ____/ /____(_)__ 
+  / /| | / / _ \| |/_/ __ `/ __ \/ __  / ___/ / _ |
+ / ___ |/ /  __/>  </ /_/ / / / / /_/ / /  / /  __/
+/_/  |_/_/\___/_/|_|\__,_/_/ /_/\__,_/_/  /_/\___/                                                    
+                                  _______________           
+                                 |               |
+                                 | By SweetKoffi | 
+                                 |_______________|'''
+
+# Taille du rectangle 
+lines = logo.split("\n")
+width = max(len(line) for line in lines)
+
+# Haut et bas du rectangle
+top_border = "+" + "-" * (width + 2) + "+"
+bottom_border = "+" + "-" * (width + 2) + "+"
+
+# Imprimer ASCII rectangle art
+def print_logo():
+    print(top_border)
+    for line in lines:
+        padding = " " * (width - len(line))
+        print("| " + line + padding + " |")
+    print(bottom_border)
+
+######################################################### Rich Table
+console = Console()
+table = Table(title="Alexandrie by SweetKoffi")
+table.add_column("Title", justify="left",width=70, style="cyan", no_wrap=True)
+table.add_column("Author",width=30, style="magenta")
+table.add_column("URL",width=70, justify="left", style="green")
+########################################################## Option 1
+#def ScholarGoogle(): 
+    #parser = CustomGoogleScholarProfiles()
+    #data = parser.scrape_google_scholar_profiles(
+    #    query='Intelligence artificielle',
+    #    pagination=False,
+    #    save_to_csv=False,
+    #    save_to_json=False
+    #)
+    #print(json.dumps(data, indent=2))
+########################################################## Option 2
+def ScholarGoogle (keyword_search_google): 
+    #base_url = 'https://serpapi.com/search.json?engine=google_scholar'
+    #https://serpapi.com/search.json?engine=google_scholar&q=biology
+    params = {
+    "engine": "google_scholar",
+    "q": keyword_search_google,
+    "api_key": serapi_api_key
+    }
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    #print(type(results))
+    #organic_results = results["organic_results"]
+    resultats_json = json.dumps(results)
+    resultats_loaded = json.loads(resultats_json)
+    organic_resultats_json = resultats_loaded["organic_results"]
+    #### Add to rich table 
+    for article in organic_resultats_json:
+        infosup = article['publication_info']
+        table.add_row(article['title'],infosup['summary'],article['link'])
+        table.add_row('-'*70 ,'-' * 30, '-'*70)
+#########################################################  Fonction HermesNewsApi
+def HermesNewsApi(keyword,date):
+    base_url = 'https://newsapi.org/v2/everything?'
+    sort_by = 'popularity'
+    api_key = news_api_key
+    #print(api_key)
+    url = f"{base_url}q={keyword}&from={date}&sortBy={sort_by}&apiKey={api_key}"
+    response = requests.get(url)
+    #json_response = json.dumps(response)
+    json_response = response.json()
+    resultats = json_response['articles']
+     #### Add to rich table  
+    for article in resultats:
+        table.add_row(article['title'],article['author'],article['url'])
+        table.add_row('-'*70 ,'-' * 30, '-'*70)
+
+###############
+keyword = input('Entrez le keyword : ')
+date    = yesterday.strftime("%y%m%d")
+###############
+print_logo()
+HermesNewsApi(keyword,date)
+ScholarGoogle(keyword)
+console.print(table)
+
---
+++ Readme.txt
@@ -0,0 +1,24 @@
+PROJET Alexandrie -------------> Heritée du Projet_Hermes_Me_V3
+
+    ## Config.txt ( gitignore ) -------------> stock API key 
+
+    ## Fonction HermesNewsApi : Utilise NEWS-API , et fait une recherche via le endpoint "/everything afin de trouver
+    les articles correspondant au mot clee rentrer via le prompteur. Verifie seulement les articles publier dans les derniers
+    24h. ( Clee API lue a partir du fichier config.tx ) " 
+
+
+    ## Fonction Alexandrie : 
+        - Option 1 - Package "google_scholar_py" -------------> need Chrome to be available on the machine used (AppData\Local\Google\Chrome\Application\chrome.exe)
+            - Back end use selenium  -------------> Option Abandonne
+    
+        - Option 2 - SERP-API key ( 100 search by day )
+            - need : pip install google-search-results -------------> Option retenue 
+
+Requirements 
+
+    - configparser
+    - requests
+    - json
+    - rich ( table , console )
+    - google-search-results
+
